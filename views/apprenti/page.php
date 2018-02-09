24e7734b25c1b0f30affe3c46dcc16e18d295e17
Bluxget <bluxget@gmail.com>
2018-02-09 11:29:52 +0100

    Avancement module Apprenti

--- views/apprenti/page.php
+++ views/apprenti/page.php
@@ -3,24 +3,18 @@
 </div>
 
 <form action="?action=modifierpage&id=<?=$datas['page']['id_page']; ?>"  method="post">
-	<div class="section">
-		<div class="left row">
-			<?=$datas['page']['contenu']; ?>
+	<div class="left row">
+		<?=$datas['page']['contenu']; ?>
+	</div>
+
+	<div class="row">
+		<div class="input-field">
+			<input id="submit" name="submit" type="submit" class="btn validate" value="Valider les modifications" />
 		</div>
 	</div>
 
-	<div class="divider"></div>
-
-	<div class="section">
-		<div class="row">
-			<div class="input-field">
-				<input id="submit" name="submit" type="submit" class="btn validate" value="Valider les modifications" />
-			</div>
-		</div>
-
-		<div class="row">
-			<h6>Page <?=$datas['page']['position']; ?></h6>
-		</div>
+	<div class="row">
+		<h6>Page <?=$datas['page']['position']; ?></h6>
 	</div>
 </form>
 
